0216324125cd0c05d5e4bd7d287baa6033f1a9ed
POO <y.huang01@ufromail.cl>
2026-04-21 14:34:46 -0400

    Clase TodaysDate

--- src/TodaysDate.java
+++ src/TodaysDate.java
@@ -1,2 +1,21 @@
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 public class TodaysDate {
+    String time;
+    public int day;
+    private int month;
+    protected int year;
+    public void printDateAndTime() {
+        GregorianCalendar calendar = new GregorianCalendar();
+        time = calendar.get(Calendar.HOUR_OF_DAY) + ":" + calendar.get(Calendar.MINUTE) + ":" + calendar.get(Calendar.SECOND);
+        day = calendar.get(Calendar.DATE);
+        month = calendar.get(Calendar.MONTH) + 1;
+        year = calendar.get(Calendar.YEAR);
+        System.out.println("Time: " + time);
+        System.out.println("Date: " + day + "/" + month + "/" + year);
+
+    }
+
+
 }
